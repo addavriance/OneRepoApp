d6368664af16ecbd9269c46dd34b41c35cf9e87b
addavriance <61160742+addavriance@users.noreply.github.com>
2025-11-21 02:54:05 +0300

    feat: improved setFlag behavior

--- src/shared/flags.ts
+++ src/shared/flags.ts
@@ -1,5 +1,6 @@
-export function setFlag(flags: number, flag: number, enable: boolean): number {
-    if (enable) {
+export function setFlag(flags: number, flag: number, enable?: boolean): number {
+    const has = hasFlag(flags, flag);
+    if (enable !== undefined && enable || !has) {
         return flags | flag;
     } else {
         return flags & ~flag;
