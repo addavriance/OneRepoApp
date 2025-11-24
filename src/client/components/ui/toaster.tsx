ccdad81257c4b4ce1fa26c77c22e992b5e030d60
addavriance <61160742+addavriance@users.noreply.github.com>
2025-11-24 18:23:30 +0300

    fear: improve toast behavior

--- src/client/components/ui/toaster.tsx
+++ src/client/components/ui/toaster.tsx
@@ -7,6 +7,7 @@ import {
     ToastViewport,
 } from "@/components/ui/toast"
 import {useToast} from "@/hooks/use-toast"
+import React from "react";
 
 export function Toaster() {
     const {toasts} = useToast()
@@ -26,7 +27,7 @@ export function Toaster() {
                         <ToastClose/>
                     </Toast>
                 )
-            })}
+            }) as React.ReactNode}
             <ToastViewport/>
         </ToastProvider>
     )
